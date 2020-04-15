21d15c764b75544175a29b423dd7294a757edfd4
Yusuke Kita <clockit@clock-it.jp>
2020-04-16 06:24:07 +0900

    lintを導入

--- laravel7/nuxt/.eslintrc.js
+++ laravel7/nuxt/.eslintrc.js
@@ -10,8 +10,14 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'eslint:recommended',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // より厳しいルールにするには`plugin:vue/strongly-recommended` もしくは `plugin:vue/recommended` に切り替えることを検討してください。
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'vue'
   ],
   // add your custom rules here
   rules: {
