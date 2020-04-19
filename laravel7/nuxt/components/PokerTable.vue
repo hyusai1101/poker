c1d33fc00139a8357035bbefb05a19733788cfd6
Yusuke Kita <clockit@clock-it.jp>
2020-04-19 13:04:27 +0900

    アクションのとりあえず実装

--- laravel7/nuxt/components/PokerTable.vue
+++ laravel7/nuxt/components/PokerTable.vue
@@ -19,7 +19,8 @@
             <table class="table table-bordered">
               <tbody>
               <tr v-for="handRange in handRanges">
-                <td v-for="hand in handRange">{{ hand.firstHand }}{{ hand.secondHand }}<span v-if="hand.suited">S</span><span v-else="hand.suited">O</span></td>
+                <td v-for="hand in handRange" @click="update(hand.id,hand.action)">{{ hand.firstHand }}{{ hand.secondHand }}<span
+                  v-if="hand.suited">s</span><span v-else="hand.suited">o</span></td>
               </tr>
               </tbody>
             </table>
@@ -28,19 +29,19 @@
             </v-card-text>
 
             <v-card-actions>
-              <v-spacer />
+              <v-spacer/>
 
               <v-btn
-color="green darken-1" text
-@click="dialog = false"
->
+                color="green darken-1" text
+                @click="dialog = false"
+              >
                 Disagree
               </v-btn>
 
               <v-btn
-color="green darken-1" text
-@click="dialog = false"
->
+                color="green darken-1" text
+                @click="dialog = false"
+              >
                 Agree
               </v-btn>
             </v-card-actions>
@@ -51,51 +52,76 @@ color="green darken-1" text
   </div>
 </template>
 <script>
-import axios from 'axios';
-import { mapGetters } from 'vuex'
-export default {
-  name: "PokerTable",
-  data() {
-    return {
-      handRanges: [],
-      dialog: false,
-    };
-  },
-  computed: {
-    ...mapGetters(['items'])
-  },
-  mounted () {
-    const baseUrl = 'http://poker.com/api/handRanges';
-    axios.get(baseUrl).then(res =>(
-      this.handRanges = res.data.data
-    ))
-  }
-};
+  import axios from 'axios';
+  import {mapGetters} from 'vuex'
+
+  export default {
+    name: "PokerTable",
+    data() {
+      return {
+        handRanges: [],
+        dialog: false,
+      };
+    },
+    computed: {
+      ...mapGetters(['items'])
+    },
+    mounted() {
+      const baseUrl = 'http://poker.com/api/handRanges';
+      axios.get(baseUrl).then(res => (
+        this.handRanges = res.data.data
+      ))
+    },
+    methods: {
+      update(handRangeId,action) {
+        // fixme リクエストアクションはどっちで担保するか決めないといけない。
+        let requestAction = '';
+        if(action === 'fold'){
+          requestAction = 'call'
+        } else if(action === 'call'){
+          requestAction = 'raise'
+        } else if(action === 'raise'){
+          requestAction = 're_raise'
+        } else {
+          requestAction = ' fold'
+        }
+        const baseUrl = 'http://poker.com/api/handRanges';
+        axios.put(baseUrl, {
+          'id': handRangeId,
+          'action': requestAction
+        }).then(response =>
+            console.log(response))
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    }
+  };
 </script>
 
 <style scoped>
-.ellipse {
-  margin: 0 auto;
-  width: 700px;
-  height: 400px;
-  position: absolute;
-  background: rgb(58, 95, 60);
-  border-radius: 50%;
-  border-style: solid;
-  border-width: 10px;
-  border-color: #000000 #000000 #000000 #000000;
-}
+  .ellipse {
+    margin: 0 auto;
+    width: 700px;
+    height: 400px;
+    position: absolute;
+    background: rgb(58, 95, 60);
+    border-radius: 50%;
+    border-style: solid;
+    border-width: 10px;
+    border-color: #000000 #000000 #000000 #000000;
+  }
 
-.human {
-  width: 200px;
-  height: 200px;
-  position: relative;
-}
+  .human {
+    width: 200px;
+    height: 200px;
+    position: relative;
+  }
 
-table th,table td{
-  color:blue;
-  border:solid 1px black;
-  background-color:yellowgreen;
-  text-align: center;
-}
+  table th, table td {
+    color: blue;
+    border: solid 1px black;
+    background-color: yellowgreen;
+    text-align: center;
+  }
 </style>
