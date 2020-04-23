13dab16ce93a70b3ae1b2ee5e0d1103a99f2b664
Yusuke Kita <clockit@clock-it.jp>
2020-04-23 11:04:30 +0900

    定数管理のファイルを追加

--- laravel7/nuxt/nuxt.config.js
+++ laravel7/nuxt/nuxt.config.js
@@ -33,7 +33,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    '@/plugins/consts.js',
+  ],
   /*
    ** Nuxt.js dev-modules
    */
---
+++ laravel7/nuxt/plugins/consts.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+Vue.prototype.$consts = {
+  // ポジションID
+  // 命名規則を付けないとやばいことになる
+  BBID: 1,
+  SBID: 2,
+  UTGID: 3,
+  EP1: 4,
+  EP2: 5,
+  MP1: 6,
+  MP2: 7,
+  HJ: 8,
+  CO: 9,
+  BTN: 10,
+}
