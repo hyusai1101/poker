c1aa0a6bf2355d556a54fc88113018b15b377b3a
Yusuke Kita <clockit@clock-it.jp>
2020-04-25 11:55:18 +0900

    ヘッダーロゴの挿入

--- laravel7/nuxt/components/common/TheHeader.vue
+++ laravel7/nuxt/components/common/TheHeader.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>ヘッダー</div>
+
+  <img  class="l-Header-Logo" src="@/assets/twitter_header_photo_2.png">
 </template>
 <script>
 export default {
   name: 'TheHeader'
 }
 </script>
+
+<style>
+  .l-Header-Logo {
+    width: 50%;
+    text-align:center;
+    display: block;
+    margin: 0 auto;
+  }
+</style>
