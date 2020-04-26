fefe3f160bd192e532307c78ab01e2557c705863
Yusuke Kita <clockit@clock-it.jp>
2020-04-26 12:11:23 +0900

    トップページと画面遷移を実装

--- laravel7/nuxt/pages/index.vue
+++ laravel7/nuxt/pages/index.vue
@@ -1,17 +1,18 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="400"
-  >
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      :src="require('@/assets/6max-hand-range.png')"
+  <a @click="$router.push('/hand-range/6')">
+    <v-card
+      class="mx-auto"
+      max-width="400"
     >
-      <v-card-title>6MAX Opening Ranges</v-card-title>
-    </v-img>
-
-  </v-card>
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="require('@/assets/6max-hand-range.png')"
+      >
+        <v-card-title>6MAX Opening Ranges</v-card-title>
+      </v-img>
+    </v-card>
+  </a>
 </template>
 
 <script>
@@ -22,11 +23,6 @@ export default {
   components: {
     Logo,
     VuetifyLogo,
-  },
-  data() {
-    return {
-      dialog: false,
-    };
   },
 };
 </script>
