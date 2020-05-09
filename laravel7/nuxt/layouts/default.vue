43834e78dad32918fb28d0f88428dc9a60c07f59
Yusuke Kita <clockit@clock-it.jp>
2020-05-09 09:07:12 +0900

    templateタグが外れていたた

--- laravel7/nuxt/layouts/default.vue
+++ laravel7/nuxt/layouts/default.vue
@@ -1,24 +1,26 @@
-<v-app class="Wrapper">
-  <div class="l-Header">
-    <div class="l-Header_Container">
-      <TheHeader />
+<template>
+  <v-app class="Wrapper">
+    <div class="l-Header">
+      <div class="l-Header_Container">
+        <TheHeader />
+      </div>
     </div>
-  </div>
-  <div class="l-Main">
-    <div class="l-Main_Container">
-      <v-content>
-        <v-container>
-          <nuxt />
-        </v-container>
-      </v-content>
+    <div class="l-Main">
+      <div class="l-Main_Container">
+        <v-content>
+          <v-container>
+            <nuxt />
+          </v-container>
+        </v-content>
+      </div>
     </div>
-  </div>
-  <div class="l-Footer">
-    <div class="l-Footer_Container">
-      <TheFooter/>
+    <div class="l-Footer">
+      <div class="l-Footer_Container">
+        <TheFooter/>
+      </div>
     </div>
-  </div>
-</v-app>
+  </v-app>
+</template>
 <script>
 import TheHeader from '@/components/common/TheHeader'
 import TheFooter from '@/components/common/TheFooter'
