f53d5a6517dddaac91f6022d980bf51c76eddf97
Yusuke Kita <clockit@clock-it.jp>
2020-05-14 23:54:31 +0900

    ヘッダー修正

--- laravel7/nuxt/components/common/TheHeader.vue
+++ laravel7/nuxt/components/common/TheHeader.vue
@@ -1,5 +1,18 @@
 <template>
-  <img class="l-Header-Logo" src="@/assets/logo_transparent.png">
+  <v-card
+    color="grey lighten-4"
+    flat
+    tile
+  >
+    <v-toolbar>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>try-poker</v-toolbar-title>
+      <v-spacer></v-spacer>
+<!--      <img class="l-Header-Logo" src="@/assets/logo_transparent.png">-->
+      <a href="./login">ログイン</a>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+  </v-card>
 </template>
 <script>
 export default {
