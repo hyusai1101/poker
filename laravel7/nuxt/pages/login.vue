d0c60722570f6fa569ef91027692aa3c37cb659f
Yusuke Kita <clockit@clock-it.jp>
2020-05-15 00:41:08 +0900

    ログインフォームの修正

--- laravel7/nuxt/pages/login.vue
+++ laravel7/nuxt/pages/login.vue
@@ -1,76 +1,71 @@
 <template>
-  <v-container>
-    <v-row
-      justify="center"
-    >
-      <v-form
-        ref="form"
-        lazy-validation
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <h2>ログイン</h2>
+    <v-text-field
+      v-model="loginInfo.email"
+      :rules="emailRules"
+      label="メールアドレス"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="loginInfo.password"
+      :rules="[rules.required, rules.min]"
+      label="パスワード"
+      required
+    ></v-text-field>
+      <v-btn
+        color="primary"
+        @click="login()"
       >
-        <v-text-field
-          v-model="loginInfo.email"
-          :rules="emailRules"
-          label="メールアドレス"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="loginInfo.password"
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required, rules.min]"
-          :type="show1 ? 'text' : 'password'"
-          label="パスワード"
-          hint="パスワードは最低8文字以上です"
-          counter
-        ></v-text-field>
-        <v-btn
-          color="primary"
-          @click="login()"
-        >
-          ログイン
-        </v-btn>
-      </v-form>
-    </v-row>
-  </v-container>
+        ログイン
+      </v-btn>
+  </v-form>
+
 </template>
 
 <script>
-import {mapActions} from 'vuex'
-import axios from 'axios'
-const Cookie = process.client ? require('js-cookie') : undefined
-export default {
-  data() {
-    return {
-      password: '',
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      show1: false,
-      rules: {
-        required: value => !!value || 'Required.',
-        min: v => v.length >= 5 || 'Min 8 characters',
-      },
-      loginInfo: {
-        email: '',
+  import {mapActions} from 'vuex'
+  import axios from 'axios'
+
+  const Cookie = process.client ? require('js-cookie') : undefined;
+  export default {
+    data() {
+      return {
         password: '',
-      }
-    };
-  },
-  methods: {
-    login() {
-      axios.post('http://poker.com/api/login/',
-        this.loginInfo
-      ).then(res =>{
-        Cookie.set('jwt', res.data.token);
-        this.$router.push('/')
-      }).catch(error => {
-        alert('ハンドレンジの変更に失敗しました。リロードして再度お試しください')
-      });
+        email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+        show1: false,
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 5 || 'Min 8 characters',
+        },
+        loginInfo: {
+          email: '',
+          password: '',
+        }
+      };
     },
-    ...mapActions([
-      'postLogin'
-    ])
+    methods: {
+      login() {
+        axios.post('http://poker.com/api/login/',
+          this.loginInfo
+        ).then(res => {
+          Cookie.set('jwt', res.data.token);
+          this.$router.push('/')
+        }).catch(error => {
+          alert('ログインに失敗しました。再度お試しください。')
+        });
+      },
+      ...mapActions([
+        'postLogin'
+      ])
+    }
   }
-}
 </script>
